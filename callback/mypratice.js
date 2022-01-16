39787d2642e430a5b091828e40e10d25fd144f42
yi-1117 <edwin821117@gmail.com>
2022-01-16 11:13:54 +0800

    add dotenv

---
+++ callback/mypratice.js
@@ -0,0 +1,23 @@
+// function sayHi(myCallback){
+//     myCallback();
+// }
+// function callme(){
+//     console.log('im back');
+// }
+// sayHi(callme);
+
+// setTimeout(function(){
+//     console.log('hi');
+// }, 1000)
+
+// setTimeout(sayHi, 1000);
+// function sayHi(){
+//     console.log('hi');
+// }
+
+let input;
+if(input === undefined){
+    doThis();
+}else{
+    doThat();
+}
